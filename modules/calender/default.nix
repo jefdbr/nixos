10bfaa1742a0e48e0c118239f5a8b4b313bbf684
Jeffrey Debaere <jeffrey.debaere@ugent.be>
2026-03-20 13:03:24 +0100

    add calender

---
+++ modules/calender/default.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  # Required services for GNOME Calendar
+  programs.dconf.enable = true;
+  services.gnome.evolution-data-server.enable = true;
+
+  # Needed for Nextcloud integration
+  services.gnome.gnome-online-accounts.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    gnome-calendar
+  ];
+}
